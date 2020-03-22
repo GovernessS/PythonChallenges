c221b46a23904123b9b1a62f914cad6954c22b39
GovernessS <35182441+GovernessS@users.noreply.github.com>
2020-03-21 23:06:41 -0500

    Update and rename tenth_power to tenth_power.py

--- tenth_power.py
+++ tenth_power.py
@@ -1,8 +1,6 @@
-# Write your tenth_power function here:
 def tenth_power(num):
   return num**10
   
-# Uncomment these function calls to test your tenth_power function:
 print(tenth_power(1))
 # 1 to the 10th power is 1
 print(tenth_power(0))
