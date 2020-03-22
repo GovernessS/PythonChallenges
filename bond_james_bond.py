a64076ccdfcf7c6034fe679a501513b12c48ef7c
GovernessS <35182441+GovernessS@users.noreply.github.com>
2020-03-21 23:17:48 -0500

    Create bond_james_bond.py

---
+++ bond_james_bond.py
@@ -0,0 +1,7 @@
+def introduction(first_name, last_name):
+  return last_name + ", " + first_name + " " + last_name
+
+print(introduction("James", "Bond"))
+# should print Bond, James Bond
+print(introduction("Maya", "Angelou"))
+# should print Angelou, Maya Angelou
