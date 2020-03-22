562ea89bdd0667f83237bec54bac2c6165989586
GovernessS <35182441+GovernessS@users.noreply.github.com>
2020-03-21 23:17:12 -0500

    Create first_three_multiples.py

---
+++ first_three_multiples.py
@@ -0,0 +1,8 @@
+def first_three_multiples(num):
+  print(num, num*2, num*3)
+  return num*3
+
+first_three_multiples(10)
+# should print 10, 20, 30, and return 30
+first_three_multiples(0)
+# should print 0, 0, 0, and return 0
